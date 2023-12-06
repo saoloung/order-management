1fa4216001c358124751b94a8a5a6588131850af
Champ <saoloung@gmail.com>
2023-12-06 17:58:15 +0700

    update EditView by add new function and layout

--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,6 +1,98 @@
+<script setup>
+import { onMounted, ref, reactive } from 'vue'
+import { RouterLink, useRoute } from 'vue-router'
+import { useOrderStore } from '../stores/order'
+
+const orderStore = useOrderStore()
+const route = useRoute()
+
+const orderId = ref(-1)
+const orderData = reactive({})
+
+onMounted(async () => {
+  orderId.value = parseInt(route.params.id)
+  await orderStore.loadOrder(orderId.value)
+
+  orderData.docNo = orderStore.selectedOrder.docNo
+  orderData.customerName = orderStore.selectedOrder.customerName
+  orderData.docStatus = orderStore.selectedOrder.docStatus
+  orderData.products = orderStore.selectedOrder.products
+  orderData.shipments = orderStore.selectedOrder.shipments
+})
+
+const editOrder = async (orderData, orderId) => {
+  await orderStore.editOrder(orderData, orderId)
+  alert('update completed')
+} 
+
+</script>
+
 <template>
   <div>
-    <h1>Edit view</h1>
+    Order Edit View
+    <RouterLink :to="{ name: 'order-list' }">Back</RouterLink>
+    <div>
+      <h2>Order Header</h2>
+      <div>
+        Order ID:
+        {{ orderId }}
+      </div>
+      <div>
+        Order no.:
+        <input type="text" v-model="orderData.docNo">
+      </div>
+      <div>
+        Customer name:
+        <input type="text" v-model="orderData.customerName">
+      </div>
+      <div>
+        Doc Status:
+        <select v-model="orderData.docStatus">
+          <option v-for="status in orderStore.statuses" :value="status">
+            {{ status }}
+          </option>
+        </select>
+      </div>
+      <button @click="editOrder(orderData, orderId)">Update</button>
+    </div>
+    <div>
+      <h2>Order Detail</h2>
+      <div>
+        <table>
+          <thead>Product
+            <tr>
+              <th>Name</th>
+              <th>Description</th>
+              <th>Pic</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="product in orderData.products">
+              <td> <input type="text" v-model="product.name"> </td>
+              <td> <input type="text" v-model="product.description"> </td>
+              <td> <img :src="product.imageUrl"></td>
+              <td> <input type="text" v-model="product.quantity"> </td>
+              <td> <input type="text" v-model="product.price"> </td>
+            </tr>
+          </tbody>
+        </table>
+        <table>
+          <thead>Shipment
+            <tr>
+              <th>Name</th>
+              <th>Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="shipment in orderData.shipments">
+              <td> <input type="text" v-model="shipment.name"> </td>
+              <td> <input type="text" v-model="shipment.quantity"> </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
-
