eefb5b5462d35969a8682184c1a34bde178f0eb9
Champ <saoloung@gmail.com>
2023-12-06 17:52:42 +0700

    update HomeView EditView CreateView

---
+++ src/views/CreateView.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+      <h1>Create view</h1>
+    </div>
+  </template>
+  
+  
--- src/views/EditView.vue
+++ src/views/EditView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>Home view</h1>
+    <h1>Edit view</h1>
   </div>
 </template>
 
